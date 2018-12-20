545433479b6d424230793431c05e6431e1d312bc
Matt Broach <go.for.dover@gmail.com>
2018-12-20 00:18:14 -0500

    fixing markdown on pypi

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ from adv import __version__
 readme = os.path.join(os.path.dirname(__file__), 'README.md')
 
 setup(
-    name='adv',
+    name='advocate-sdk',
     version=__version__,
     url='https://github.com/AdvocatesInc/advocate-python-sdk',
     author='Advocates, Inc',
     author_email='admin@adv.gg',
     description='SDK for developing interactive widgets and more on the Advocate platform',
     long_description=open(readme).read(),
+    long_description_content_type="text/markdown",
     license='Proprietary and confidential',
     zip_safe=False,
     packages=find_packages(),
